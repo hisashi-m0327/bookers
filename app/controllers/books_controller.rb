4971c954cbad758ec6cd10cc3eefb0bae89d03a4
HisashiMiyashita <225394446+hisashi-m0327@users.noreply.github.com>
2025-09-12 15:19:02 +0000

    [Add] bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,9 +14,15 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
   end
 
 private
